d0e91fe8ee57bbca9616db8731ac473e1f810cdd
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-09-26 22:09:42 +0500

    add email.js

--- src/modules/email.js
+++ src/modules/email.js
@@ -1,1 +1,20 @@
-const nodemailer = req
+const nodemailer = require("nodemailer");
+
+async function email(to,mail_body, mail_html){
+    const transport = await nodemailer.createTransport({
+        host: "smtp.yandex.ru",
+        port: 465,
+        secure:true,
+        auth:{
+            user: "rustambek01uz",
+            pass: "931487733RMS",
+        },
+    });
+return await trnasport.sendEmail({
+    from: '"Bizning kompaniya" <foo@example.com>',
+    to, subject,
+    text: mail_body,
+    html: mail_html,
+});
+
+}
