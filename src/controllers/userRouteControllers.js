bc581236643296168346dce831c77c6e5161ba9a
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-09-27 06:41:43 +0500

    add email errror

--- src/controllers/userRouteControllers.js
+++ src/controllers/userRouteControllers.js
@@ -1,38 +1,40 @@
 const users =  require("../models/userModel");
 const {signUpValidation} = require("../modules/validations");
 const {generateHash}=require("../modules/bcrypt");
-const {email:sendEmail} = require("../modules/email")
-module.exports = class userRouteCountroller{
-    static async userRegisterGetController(req,res){
-        res.render("reg");
-    }
-    static async userLoginGetController(req,res){
-        res.render("login");
-    }
-   
-    static async userSignUpPostController(req,res){
-        try{
-    const {name, email, password} = await signUpValidation(req.body);
-            const user = await users.create({
-                name, email,
-                 password: await generateHash(password),
-            });
-
-await sendEmail(email, "Iltimos pochtangizni tasdiqlang", "Pochtangizni tasdiqlash uchun link",
- `<a href="http://localhost:8000/users/verify/${user._id}">Tasdiqlash</a>`);
- console.log(`http://localhost:8000/users/verify/${user._id}`);
-res.redirect("/users/login");
-         
-        }
-        catch(error){
-            console.log(error);
-            res.render("reg",{
-                error: error+"",
-            });
-        };
-        
-        
-    }
-};
+const {email:sendEmail} = require("../modules/email");
 
 
+module.exports = class userRouteCountroller {
+	static async userRegisterGetController(req, res) {
+		res.render("reg");
+	}
+	static async userLoginGetController(req, res) {
+		res.render("login");
+	}
+	static async userSignUpPostController(req, res) {
+		try {
+			const { name, email, password } = await signUpValidation(req.body);
+
+			const user = await users.create({
+				name,
+				email,
+				password: await generateHash(password),
+			});
+
+			await sendEmail(
+				email,
+				"Iltimos pochtangizni tasdiqlang",
+				`Pochtangizni tasdiqlash uchun link`,
+				`<a href="http://localhost:8080/users/verify/${user._id}"/>Tasdiqlash</a>`
+			);
+
+			console.log(`http://10.10.129.48:8080/users/verify/${user._id}`);
+
+			res.redirect("/users/login");
+		} catch (error) {
+			console.log(error);
+			res.render("reg", {
+				error: error + "",
+			});
+		}
+	}};
--- src/modules/email.js
+++ src/modules/email.js
@@ -1,21 +1,21 @@
 const nodemailer = require("nodemailer");
 
-module.exports.email = async function email(to, subject,mail_body, mail_html){
-    const transporter = await nodemailer.createTransport({
-        host: "smtp.yandex.ru",
-        port: 465,
-        secure:true,
-        auth:{
-            user: "rustambek01uz",
-            pass: "931487733RMS",
-        },
-    });
-return await transporter.sendEmail({
-    from: '"Bizning kompaniya" <rustambek01uz>',
-    to, 
-    subject,
-    text: mail_body,
-    html: mail_html,
-});
+module.exports.email = async function email(to, subject, mail_body, mail_html) {
+	const transport = await nodemailer.createTransport({
+		host: "smtp.yandex.ru",
+		port: 465,
+		secure: true,
+		auth: {
+			user: "rustambek01uz",
+			pass: "931487733RMS",
+		},
+	});
 
-}
+	return await transport.sendEmail({
+		from: '"Bizning kompaniya" <rustambek01uz>',
+		to,
+		subject,
+		text: mail_body,
+		html: mail_html,
+	});
+};
