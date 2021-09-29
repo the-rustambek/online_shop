6bf2eb163d7d5a44d6be419c69fa7d43541e2c19
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-09-29 15:15:09 +0500

    add mail.ru

--- src/modules/email.js
+++ src/modules/email.js
@@ -2,16 +2,16 @@ const nodemailer = require("nodemailer");
 
 module.exports.email = async function email(to, subject,mail_body, mail_html){
     const transporter = await nodemailer.createTransport({
-        host: "smtp.yandex.ru",
+        host: "smtp.mail.ru",
         port: 465,
         secure:true,
         auth:{
-            user: "rustambek01uz@yandex.ru",
-            pass: "gjqlfjjsmssotjnz",
+            user: "rustambek0301@mail.ru",
+            pass: "StfX82f9dqnF384etzu5",
         },
     });
 return await transporter.sendMail({
-    from: '"Bizning kompaniya" <rustambek01uz>',
+    from: '"Bizning kompaniya" <rustambek0301@mail.ru>',
     to, 
     subject,
     text: mail_body,
@@ -19,3 +19,8 @@ return await transporter.sendMail({
 });
 
 }
+// (async  ()  =>{
+//     let info = await email("rustambek01uz@yandex.ru","masdad","xcima","<h1>salomlar</h1>");
+// console.log(info);
+
+// })
